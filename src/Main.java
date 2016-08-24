f3ee5caf6be5b0c8a9ccd7b97edf43add933e503
Jimmy Bush <jebush94@hotmail.com>
2016-08-24 14:14:34 -0400

    Version 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
 
         MenuService menuService = new MenuService();
-        AnimalsService animalsService = new AnimalsService();
+        AnimalRepository animalRepository = new AnimalRepository("animals.json");
+        AnimalsService animalsService = new AnimalsService(animalRepository);
 
 
         while(true) {
