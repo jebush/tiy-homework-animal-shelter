56b0cff91ce5c4ce75bf7838aac07bb306d8d26c
Jimmy Bush <jebush94@hotmail.com>
2016-08-29 09:06:39 -0400

    Version 1.3

---
+++ test/animalServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class animalServiceTest{
+
+    @Test
+    public void getListOfAnimalsTest() throws IOException{
+
+        //arrange
+        AnimalRepository animalRepository = new AnimalRepository("test.json");
+
+
+
+        //act
+
+        //assert
+
+
+    }
+
+}
